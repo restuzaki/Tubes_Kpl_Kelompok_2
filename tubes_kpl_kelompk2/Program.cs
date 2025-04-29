853e3b0a10353edf8af4ffaf33c4f07ebc0ea0e4
Damai <damaiputrayudha1@gmail.com>
2025-04-29 17:19:54 +0700

    inisialisasi tubes

---
+++ tubes_kpl_kelompk2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+   
+
+    public static void Main(string[] args)
+    {
+        for (; ; ) 
+        {
+            Console.WriteLine("aji Ganteng");
+        }
+    }
+}
